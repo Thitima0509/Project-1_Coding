6a61a8553f5f0759e1566740ed97502941816110
Thitima0509 <thitimakriklong@gmail.com>
2024-12-04 16:07:21 +0700

    Add files via upload

---
+++ 1form_32piglet.py
@@ -0,0 +1,10 @@
+print("Welcome to 4/4")
+
+print("\n\nสวัสดีค่ะ กรอกข้อมูลตัวเองค่ะ")
+nickname = input("ชื่อเล่น")
+id_student = input("รหัสนักเรียน")
+service = input("ยืมหนังสือ")
+
+print("\n\nยินดีต้อนรับ", nickname)
+print("รหัสนักเรียน", id_student , "\กำลังเข้าใช้บริการ", service)
+print("\n\nขอบคุณที่เข้าใช้บริการ แล้วพบกันใหม่ค่ะ")
